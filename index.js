145bc67d9042736acb9a22b57a22d91bcd78867f
Moringa School <sylvesterkonana005@gmail.com>
2026-01-07 12:38:34 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,47 +1,72 @@
 const bookStore = {
-    name: 'Flatbooks Technical Books',
-    books: [
-        {
-            id:1,
-            title: 'Eloquent JavaScript: A Modern Introduction to Programming',
-            author: 'Marjin Haverbeke',
-            imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/51IKycqTPUL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg',
-            
-        },
-        {
-            id:2,
-            title: 'JavaScript & JQuery: Interactive Front-End Web Development',
-            author: 'Jon Duckett',
-            imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/31SRWF+LkKL._SX398_BO1,204,203,200_.jpg'
-        },
-        {
-            id:3,
-            title: 'JavaScript: The Good Parts',
-            author: 'Douglas Crockford',
-            imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/5131OWtQRaL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg',
-        },
-        {
-            id:4,
-            title: 'JavaScript: The Definitive Guide',
-            author: 'David Flanagan',
-            imageUrl: "https://images-na.ssl-images-amazon.com/images/I/51wijnc-Y8L._SX379_BO1,204,203,200_.jpg"
-            
-        },
-        {
-            id:5,
-            title: 'You Don’t Know JS',
-            author: 'Kyle Simpson',
-            imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/41T5H8u7fUL._SX331_BO1,204,203,200_.jpg'
-        },
-        {
-            id:6,
-            title: 'Cracking the Coding Interview',
-            author: 'Gayle Laakmann McDowell',
-            imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/41oYsXjLvZL._SY344_BO1,204,203,200_.jpg'
-            
-        }
-    ]
-}
-
+  name: "Flatbooks Technical Books",
+  books: [
+    {
+      id: 1,
+      title: "Eloquent JavaScript: A Modern Introduction to Programming",
+      author: "Marjin Haverbeke",
+      imageUrl:
+        "https://images-na.ssl-images-amazon.com/images/I/51IKycqTPUL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg",
+    },
+    {
+      id: 2,
+      title: "JavaScript & JQuery: Interactive Front-End Web Development",
+      author: "Jon Duckett",
+      imageUrl:
+        "https://images-na.ssl-images-amazon.com/images/I/31SRWF+LkKL._SX398_BO1,204,203,200_.jpg",
+    },
+    {
+      id: 3,
+      title: "JavaScript: The Good Parts",
+      author: "Douglas Crockford",
+      imageUrl:
+        "https://images-na.ssl-images-amazon.com/images/I/5131OWtQRaL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg",
+    },
+    {
+      id: 4,
+      title: "JavaScript: The Definitive Guide",
+      author: "David Flanagan",
+      imageUrl:
+        "https://images-na.ssl-images-amazon.com/images/I/51wijnc-Y8L._SX379_BO1,204,203,200_.jpg",
+    },
+    {
+      id: 5,
+      title: "You Don’t Know JS",
+      author: "Kyle Simpson",
+      imageUrl:
+        "https://images-na.ssl-images-amazon.com/images/I/41T5H8u7fUL._SX331_BO1,204,203,200_.jpg",
+    },
+    {
+      id: 6,
+      title: "Cracking the Coding Interview",
+      author: "Gayle Laakmann McDowell",
+      imageUrl:
+        "https://images-na.ssl-images-amazon.com/images/I/41oYsXjLvZL._SY344_BO1,204,203,200_.jpg",
+    },
+  ],
+};
 // Write your code here!
+let  header = document.querySelector("h1");
+header.textContent = bookStore.name;
 
+let bookList = document.getElementById("book-list");
+bookStore.books.forEach(function (book) {
+
+  let bookContainer = document.createElement("div");
+  bookContainer.className = "book";
+
+  let bookTitle = document.createElement("h3");
+  bookTitle.textContent = book.title;
+
+  let bookAuthor = document.createElement("p");
+  bookAuthor.textContent = book.author;
+
+  let bookImage = document.createElement("img");
+  bookImage.src = book.imageUrl;
+
+
+  bookContainer.appendChild(bookTitle);
+  bookContainer.appendChild(bookAuthor);
+  bookContainer.appendChild(bookImage);
+  bookList.appendChild(bookContainer);
+});
